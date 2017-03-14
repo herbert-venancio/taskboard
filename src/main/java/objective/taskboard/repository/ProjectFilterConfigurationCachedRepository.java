b8043f487d7c18d10c8f0a0d4737ce82b243dd9e
takeuchi <g.takeuchi@gmail.com>
2017-03-14 18:22:23 -0300

    Fixed cache issue after adding new project filter

--- src/main/java/objective/taskboard/repository/ProjectFilterConfigurationCachedRepository.java
+++ src/main/java/objective/taskboard/repository/ProjectFilterConfigurationCachedRepository.java
@@ -59,5 +59,6 @@ public class ProjectFilterConfigurationCachedRepository {
 
     public void save(ProjectFilterConfiguration f) {
         projectFilterRepository.save(f);
+        loadCache();
     }
 }
