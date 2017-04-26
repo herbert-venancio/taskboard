b906b66710d16d7599338bf7e0dbeba2926bb07d
jytakano <jean.takano@objective.com.br>
2017-04-26 11:37:39 -0300

    wip validator refactory and unit test

--- src/main/java/objective/taskboard/controller/WipValidatorController.java
+++ src/main/java/objective/taskboard/controller/WipValidatorController.java
@@ -1,8 +1,30 @@
 package objective.taskboard.controller;
 
+/*-
+ * [LICENSE]
+ * Taskboard
+ * ---
+ * Copyright (C) 2015 - 2017 Objective Solutions
+ * ---
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * [/LICENSE]
+ */
+
 import static java.util.stream.Collectors.toList;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.PRECONDITION_FAILED;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -61,53 +83,70 @@ public class WipValidatorController {
     @RequestMapping
     public ResponseEntity<WipValidatorResponse> validate(@QueryParam("issue") String issue,
             @QueryParam("user") String user, @QueryParam("status") String status) {
-
         WipValidatorResponse response = new WipValidatorResponse();
 
-        Issue i = jiraService.getIssueByKeyAsMaster(issue);
-        if (i == null) {
-            response.errorMessage = "Issue " + issue + " not found";
-            return new ResponseEntity<WipValidatorResponse>(response, BAD_REQUEST);
-        }
+        try {
+            Issue i;
+            try {
+                i = jiraService.getIssueByKeyAsMaster(issue);
+            } catch (Exception e) {
+                response.message = "Issue " + issue + " not found (" + (e.getMessage() == null ? e.toString() : e.getMessage()) + ")";
+                return new ResponseEntity<WipValidatorResponse>(response, PRECONDITION_FAILED);
+            }
 
-        if (user == null || user.isEmpty()) {
-            response.errorMessage = "Query parameter 'user' is required";
-            return new ResponseEntity<WipValidatorResponse>(response, BAD_REQUEST);
-        }
+            if (i == null) {
+                response.message = "Issue " + issue + " not found";
+                return new ResponseEntity<WipValidatorResponse>(response, PRECONDITION_FAILED);
+            }
 
-        if (status == null || status.isEmpty()) {
-            response.errorMessage = "Query parameter 'status' is required";
-            return new ResponseEntity<WipValidatorResponse>(response, BAD_REQUEST);
-        }
+            if (user == null || user.isEmpty()) {
+                response.message = "Query parameter 'user' is required";
+                return new ResponseEntity<WipValidatorResponse>(response, PRECONDITION_FAILED);
+            }
 
-        if (isClassOfServiceExpedite(i))
+            if (status == null || status.isEmpty()) {
+                response.message = "Query parameter 'status' is required";
+                return new ResponseEntity<WipValidatorResponse>(response, PRECONDITION_FAILED);
+            }
+
+            if (isClassOfServiceExpedite(i)) {
+                response.message = "Class of service is " + CLASS_OF_SERVICE_EXPEDITE;
+                return new ResponseEntity<WipValidatorResponse>(response, OK);
+            }
+
+            WipConfiguration wipConfig = getWipConfig(user, i.getProject().getKey(), status);
+            if (wipConfig == null) {
+                response.message = "No wip configuration was found";
+                return new ResponseEntity<WipValidatorResponse>(response, OK);
+            }
+
+            List<String> userTeamsWip = userTeamRepo.findByTeam(wipConfig.getTeam()).stream()
+                    .map(u -> u.getUserName())
+                    .collect(toList());
+
+            Team team = teamRepo.findByName(wipConfig.getTeam());
+            List<String> projectTeams = projectTeamRepo.findByIdTeamId(team.getId()).stream()
+                    .map(p -> p.getProjectKey())
+                    .collect(toList());
+
+            Integer wipActual = jiraSearchService
+                    .searchIssues("assignee in ('" + String.join(",", userTeamsWip) + "') " +
+                            "and project in ('" + String.join(",", projectTeams) + "') " +
+                            "and status = '" + status + "' " +
+                            (i.getIssueType().isSubtask() ?
+                                    "and issuetype in subTaskIssueTypes()" :
+                                    "and issuetype in standardIssueTypes()"))
+                    .size();
+
+            response.team = wipConfig.getTeam();
+            response.wipConfig = wipConfig.getWip();
+            response.wipActual = wipActual;
+
             return new ResponseEntity<WipValidatorResponse>(response, OK);
-
-        WipConfiguration wipConfig = getWipConfig(user, i.getProject().getKey(), status);
-        if (wipConfig == null)
-            return new ResponseEntity<WipValidatorResponse>(response, OK);
-
-        List<String> userTeamsWip = userTeamRepo.findByTeam(wipConfig.getTeam())
-                .stream().map(u -> u.getUserName())
-                .collect(toList());
-        Team team = teamRepo.findByName(wipConfig.getTeam());
-        List<String> projectTeams = projectTeamRepo.findByIdTeamId(team.getId())
-                .stream().map(p -> p.getProjectKey())
-                .collect(toList());
-
-        Integer wipActual = jiraSearchService.searchIssues(
-                "assignee in ('" + String.join(",", userTeamsWip) + "') " +
-                "and project in ('" + String.join(",", projectTeams) + "') " +
-                "and status = '" + status + "' " +
-                (i.getIssueType().isSubtask() ?
-                    "and issuetype in subTaskIssueTypes()" :
-                    "and issuetype in standardIssueTypes()")).size();
-
-        response.team = wipConfig.getTeam();
-        response.wipConfig = wipConfig.getWip();
-        response.wipActual = wipActual;
-
-        return new ResponseEntity<WipValidatorResponse>(response, OK);
+        } catch (Exception e) {
+            response.message = e.getMessage() == null ? e.toString() : e.getMessage();
+            return new ResponseEntity<WipValidatorResponse>(response, INTERNAL_SERVER_ERROR);
+        }
     }
 
     private boolean isClassOfServiceExpedite(Issue issue) {
@@ -126,32 +165,29 @@ public class WipValidatorController {
     private WipConfiguration getWipConfig(String user, String project, String status) {
         List<WipConfiguration> wipConfigs = new ArrayList<WipConfiguration>();
         List<UserTeam> userTeams = userTeamRepo.findByUserName(user);
-        for (UserTeam userTeam : userTeams) {
+        userTeams.stream().forEach((userTeam) -> {
             Team team = teamRepo.findByName(userTeam.getTeam());
             if (team == null)
-                continue;
+                return;
 
             List<ProjectTeam> projectTeams = projectTeamRepo.findByIdProjectKeyAndIdTeamId(project, team.getId());
 
             if (projectTeams.isEmpty())
-                continue;
+                return;
             wipConfigs.addAll(wipConfigRepo.findByTeamAndStatus(team.getName(), status));
-        }
+        });
 
         if (wipConfigs.isEmpty())
             return null;
 
-        wipConfigs.stream().sorted((w1, w2) -> {
-            if (w1 == null && w2 == null)
-                return 0;
-            if (w1 == null)
-                return 1;
-            if (w2 == null)
-                return -1;
+        List<WipConfiguration> wipsSorted = wipConfigs.stream().sorted((w1, w2) -> {
+            if (w1 == null && w2 == null) return 0;
+            if (w1 == null) return 1;
+            if (w2 == null) return -1;
             return w1.getWip().compareTo(w2.getWip());
         }).collect(toList());
 
-        return wipConfigs.get(0);
+        return wipsSorted.get(0);
     }
 
 }
