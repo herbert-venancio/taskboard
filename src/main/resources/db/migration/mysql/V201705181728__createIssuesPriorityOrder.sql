77fcd6b8a04580960b67a74d177ae46629a9aa62
takeuchi <g.takeuchi@gmail.com>
2017-05-24 11:46:11 -0300

    [TOOLS-10] fixing mysql migration

--- src/main/resources/db/migration/mysql/V201705181728__createIssuesPriorityOrder.sql
+++ src/main/resources/db/migration/mysql/V201705181728__createIssuesPriorityOrder.sql
@@ -1,5 +1,5 @@
-CREATE TABLE TASKBOARD_ISSUE (
+CREATE TABLE taskboard_issue (
 	issue_key VARCHAR(255) NOT NULL,
-	priority INT NOT NULL,
+	priority BIGINT NOT NULL,
 	CONSTRAINT issueprioritorder_pk PRIMARY KEY (issue_key)
 )
