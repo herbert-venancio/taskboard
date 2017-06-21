da1211e766b664167cc4cbf0c60865b221d3de39
takeuchi <g.takeuchi@gmail.com>
2017-06-21 11:53:34 -0300

    [TOOLS-30] Fixed test name to be coherent

--- src/test/java/objective/taskboard/it/SearchInputIT.java
+++ src/test/java/objective/taskboard/it/SearchInputIT.java
@@ -25,7 +25,7 @@ import org.junit.Test;
 
 public class SearchInputIT extends AuthenticatedIntegrationTest {
     @Test
-    public void whenIssueKeyIsTyped_onlyOneIssueShouldBeFound() {
+    public void whenIssueKeyIsTyped_onlyMatchingIssuesShouldBeFound() {
         MainPage mainPage = MainPage.produce(webDriver);
         mainPage.typeSearch("TASKB-61");
         mainPage.assertVisibleIssues("TASKB-610", "TASKB-611", "TASKB-612", "TASKB-613");
