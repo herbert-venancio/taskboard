97216da91cbdfaafa04635cbcc4743c2e7f872ac
jean.takano <jean.takano@objective.com.br>
2017-06-27 15:45:00 -0300

    [TOOLS-121] Filter by Release Integration Test when have no releases

--- src/test/java/objective/taskboard/it/MainPage.java
+++ src/test/java/objective/taskboard/it/MainPage.java
@@ -1,5 +1,6 @@
 package objective.taskboard.it;
 
+import static java.util.stream.Collectors.toList;
 import static org.apache.commons.lang3.StringUtils.join;
 import static org.junit.Assert.assertEquals;
 import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementValue;
@@ -8,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -44,11 +44,19 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 public class MainPage extends AbstractUiPage {
     @FindBy(css=".nameButton.user-account")
     private WebElement userLabelButton;
-    
-    @FindBy(id="searchIssues")
+
+    @FindBy(id = "searchIssues")
     private WebElement searchIssuesInput;
 
-    
+    @FindBy(id = "searchRelease")
+    private WebElement searchReleaseDropdown;
+
+    @FindBy(css = ".menuLink")
+    private WebElement menuFiltersButton;
+
+    @FindBy(tagName = "aspects-filter")
+    private WebElement aspectsFilterButton;
+
     public MainPage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -71,6 +79,35 @@ public class MainPage extends AbstractUiPage {
         waitUntil(textToBePresentInElementValue(searchIssuesInput, searchValue));
     }
 
+    public MainPage openMenuFilters() {
+        waitVisibilityOfElement(menuFiltersButton);
+        menuFiltersButton.click();
+        return this;
+    }
+
+    public MainPage openAspectsFilter() {
+        waitVisibilityOfElement(aspectsFilterButton);
+        aspectsFilterButton.click();
+        return this;
+    }
+
+    public MainPage clickCheckAllProjectFilter() {
+        WebElement checkAllProject = webDriver
+            .findElements(By.cssSelector(".aspect-item-filter.config-item-title")).stream()
+            .filter(webEl -> webEl.getText().equals("Project"))
+            .map(webEl -> webEl.findElement(By.id("checkAll")))
+            .findFirst().orElse(null);
+        if (checkAllProject == null)
+            throw new IllegalArgumentException("Element \"checkAll\" of project filter not found");
+        checkAllProject.click();
+        return this;
+    }
+
+    public MainPage assertLabelRelease(String expected) {
+        waitTextInElement(searchReleaseDropdown, expected);
+        return this;
+    }
+
     public void assertVisibleIssues(String ... expectedIssueKeyList) {
         waitUntil(ExpectedConditions.numberOfElementsToBe(By.cssSelector("paper-material.issue"), expectedIssueKeyList.length));
         List<WebElement> findElements = webDriver.findElements(By.cssSelector("paper-material.issue"));
@@ -86,7 +123,7 @@ public class MainPage extends AbstractUiPage {
     public TestIssue issue(String issueKey) {
         List<WebElement> elements = webDriver.findElements(By.cssSelector("paper-material.issue")).stream()
             .filter(webEl -> hasChildThatMatches(webEl, By.cssSelector("[data-issue-key='"+issueKey+"']")))
-            .collect(Collectors.toList());
+            .collect(toList());
         
         if (elements.size() > 1)
             throw new IllegalArgumentException("More than a single match was found");
