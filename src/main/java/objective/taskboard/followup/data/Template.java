425834703463ebec985efb1457a11863f2c7bee9
Herbert Weigert Venancio <herbert.venancio@objective.com.br>
2017-07-13 17:20:09 -0300

    fixed mysql db migration issues

--- src/main/java/objective/taskboard/followup/data/Template.java
+++ src/main/java/objective/taskboard/followup/data/Template.java
@@ -42,7 +42,7 @@ public class Template {
     private String path;
 
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "TEMPLATE_PROJETO",
+    @JoinTable(name = "TEMPLATE_PROJECT",
                 joinColumns = { @JoinColumn(name = "TemplateId",
                         nullable = false, updatable = false) },
                 inverseJoinColumns = { @JoinColumn(name = "ProjectId",
