89119d3248d8a80ca2c7f504ac1e56cf0560e465
Mayko Oliveira <maykoone@gmail.com>
2018-01-10 13:22:49 -0200

    [TOOLS-486] followup dashboard pie-chart planned vs ballpark

---
+++ src/main/java/objective/taskboard/controller/FollowUpPlannedBallparkController.java
@@ -0,0 +1,46 @@
+package objective.taskboard.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import objective.taskboard.followup.FollowupCluster;
+import objective.taskboard.followup.FollowupClusterProvider;
+import objective.taskboard.followup.PlannedVsBallparkDataAccumulator;
+import objective.taskboard.followup.impl.FollowUpDataProviderFromCurrentState;
+import objective.taskboard.repository.ProjectFilterConfigurationCachedRepository;
+
+
+@RestController
+public class FollowUpPlannedBallparkController {
+    
+    @Autowired
+    private FollowUpDataProviderFromCurrentState followUpDataProviderFromCurrentState;
+    
+    @Autowired
+    private FollowupClusterProvider followupClusterProvider;
+    
+    @Autowired
+    private ProjectFilterConfigurationCachedRepository projects;
+    
+    @RequestMapping(value = "/api/projects/{projectKey}/followup/planned-ballpark", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> data(@PathVariable("projectKey") String projectKey) {
+        if (!projects.exists(projectKey))
+            return new ResponseEntity<>("Project not found", HttpStatus.NOT_FOUND);
+        
+        Optional<FollowupCluster> followupCluster = followupClusterProvider.getForProject(projectKey);
+        if (!followupCluster.isPresent())
+            return new ResponseEntity<>("No follow-up cluster configuration available", HttpStatus.INTERNAL_SERVER_ERROR);
+        
+        final PlannedVsBallparkDataAccumulator accumulator = new PlannedVsBallparkDataAccumulator();
+        followUpDataProviderFromCurrentState.getJiraData(followupCluster.get(), projectKey).forEachRow(accumulator::accumulate);
+
+        return ResponseEntity.ok(accumulator.getData());
+    }
+}
---
+++ src/main/java/objective/taskboard/followup/PlannedVsBallparkChartData.java
@@ -0,0 +1,11 @@
+package objective.taskboard.followup;
+
+public class PlannedVsBallparkChartData {
+    public String type;
+    public double totalEffort;
+    
+    public PlannedVsBallparkChartData(String type, double totalEffort) {
+        this.type = type;
+        this.totalEffort = totalEffort;                
+    }
+}
