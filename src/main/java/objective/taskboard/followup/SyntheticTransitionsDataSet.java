3af280ecf51f4e73c589bc4b6d2b9bbe34e92cf9
Herbert Weigert Venancio <herbert.venancio@objective.com.br>
2018-01-12 10:11:18 -0200

    [TOOLS-485] bugfix IndexOutOfBoundsException when no data

--- src/main/java/objective/taskboard/followup/SyntheticTransitionsDataSet.java
+++ src/main/java/objective/taskboard/followup/SyntheticTransitionsDataSet.java
@@ -4,7 +4,13 @@ import java.util.List;
 
 public class SyntheticTransitionsDataSet extends TransitionDataSet<SyntheticTransitionsDataRow> {
 
+    private static final int INITIAL_INDEX_STATUS_HEADER = 2;
+
     public SyntheticTransitionsDataSet(String issueType, List<String> headers, List<SyntheticTransitionsDataRow> rows) {
         super(issueType, headers, rows);
+    }
+
+    public int getInitialIndexStatusHeaders() {
+        return INITIAL_INDEX_STATUS_HEADER;
     }
 }
