330444d0bfa3de65aaed41d8b2d1a28d65931268
Jhony Pacheco Gomes <jhony.gomes@objective.com.br>
2018-02-01 13:20:20 -0200

    [TOOLS-537] Sizing Import: Google API errors handling

--- src/main/java/objective/taskboard/google/SpreadsheetsManager.java
+++ src/main/java/objective/taskboard/google/SpreadsheetsManager.java
@@ -1,10 +1,16 @@
 package objective.taskboard.google;
 
 import static java.util.stream.Collectors.toList;
+import static objective.taskboard.utils.ThreadUtils.sleepOrCry;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.util.backoff.BackOffExecution;
+import org.springframework.util.backoff.ExponentialBackOff;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.services.sheets.v4.Sheets.Spreadsheets;
@@ -15,16 +21,18 @@ import com.google.api.services.sheets.v4.model.Spreadsheet;
 import com.google.api.services.sheets.v4.model.ValueRange;
 
 public class SpreadsheetsManager {
-	
-	private final Spreadsheets spreadsheets;
-	
-    public SpreadsheetsManager(Spreadsheets spreadsheets) {
-		this.spreadsheets = spreadsheets;
-	}
 
-	public List<List<Object>> readRange(String spreadsheetId, String range) throws SpreadsheeNotFoundException {
-	    ValueRange valueRange = handleException(spreadsheetId, () -> spreadsheets.values().get(spreadsheetId, range).execute());
-	    return valueRange.getValues();
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(SpreadsheetsManager.class);
+
+    private final Spreadsheets spreadsheets;
+
+    public SpreadsheetsManager(Spreadsheets spreadsheets) {
+        this.spreadsheets = spreadsheets;
+    }
+
+    public List<List<Object>> readRange(String spreadsheetId, String range) throws SpreadsheeNotFoundException {
+        ValueRange valueRange = handleException(spreadsheetId, () -> spreadsheets.values().get(spreadsheetId, range).execute());
+        return valueRange.getValues();
     }
 
     public int getSheetId(String spreadsheetId, String sheetTitle) {
@@ -41,11 +49,7 @@ public class SpreadsheetsManager {
     }
 
     public void batchUpdate(String spreadsheetId, BatchUpdateSpreadsheetRequest content) {
-        try {
-            spreadsheets.batchUpdate(spreadsheetId, content).execute();
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
+        handleException(spreadsheetId, () -> spreadsheets.batchUpdate(spreadsheetId, content).execute());
     }
 
     public List<String> getSheetsTitles(String spreadsheetId) {
@@ -69,32 +73,82 @@ public class SpreadsheetsManager {
     private Spreadsheet getSpreadsheet(String spreadsheetId) {
         return handleException(spreadsheetId, () -> spreadsheets.get(spreadsheetId).execute());
     }
-    
+
     private <T> T handleException(String spreadsheetId, SpreadsheetsRequest<T> request) {
         try {
             return request.execute();
-
         } catch (GoogleJsonResponseException ex) {
-            if (ex.getStatusCode() == 404)
+            if (ex.getStatusCode() == 401)
+                throw new SpreadsheeInvalidCredentialsException();
+            else if (ex.getStatusCode() == 403)
+                throw new SpreadsheeDailyLimitException();
+            else if (ex.getStatusCode() == 404)
                 throw new SpreadsheeNotFoundException(spreadsheetId);
-            
+            else if (ex.getStatusCode() >= 500)
+                exponentialBackOff(() -> request.execute());
+
             throw new RuntimeException(ex);
 
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
     }
-    
+
+    private <T> T exponentialBackOff(SpreadsheetsRequest<T> request) {
+        ExponentialBackOff exponentialBackOff = new ExponentialBackOff(3000L, 1.5);
+        exponentialBackOff.setMaxElapsedTime(30000);
+        BackOffExecution exec = exponentialBackOff.start();
+
+        while(true) {
+            try {
+                return request.execute();
+            } catch (IOException e) {
+                long timeToWait = exec.nextBackOff();
+                if (timeToWait == BackOffExecution.STOP)
+                    throw new SpreadsheeBackendException();
+
+                log.info("Execution failed, will retry after" + " " + timeToWait + " ms");
+                sleepOrCry(timeToWait);
+            }
+        }
+    }
+
     private interface SpreadsheetsRequest<T> {
         T execute() throws IOException;
     }
-    
-    public static class SpreadsheeNotFoundException extends RuntimeException {
-        private static final long serialVersionUID = 4074725260811075034L;
-        
-        public SpreadsheeNotFoundException(String spreadsheetId) {
-            super("Spreadsheet with id '" + spreadsheetId + "' not found");
+
+    public static class SpreadsheetException extends RuntimeException {
+        private static final long serialVersionUID = 1656139289656324685L;
+
+        public SpreadsheetException() {}
+
+        public SpreadsheetException(String message) {
+            super(message);
         }
+    }
+
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public static class SpreadsheeNotFoundException extends SpreadsheetException {
+        private static final long serialVersionUID = 4074725260811075034L;
+
+        public SpreadsheeNotFoundException(String spreadsheetId) {
+            super("Spreadsheet with id '" + spreadsheetId + "' not found.");
+        }
+    }
+
+    @ResponseStatus(value = HttpStatus.SERVICE_UNAVAILABLE, reason="We are having communication issues with Google Spreadsheet. Please, try again in 15 minutes.")
+    public static class SpreadsheeBackendException extends SpreadsheetException {
+        private static final long serialVersionUID = 6453134117524063963L;
+    }
+
+    @ResponseStatus(value = HttpStatus.FORBIDDEN, reason="Daily limit exceeded. Please, try again tomorrow.")
+    public static class SpreadsheeDailyLimitException extends SpreadsheetException {
+        private static final long serialVersionUID = 6179253232804985065L;
+    }
+
+    @ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason="Invalid Credentials.")
+    public static class SpreadsheeInvalidCredentialsException extends SpreadsheetException {
+        private static final long serialVersionUID = -5332931549640852185L;
     }
 
 }
---
+++ src/main/java/objective/taskboard/utils/ThreadUtils.java
@@ -0,0 +1,19 @@
+package objective.taskboard.utils;
+
+public class ThreadUtils {
+
+    public static void sleepOrCry(long timeToWait) {
+        sleepOrCry(timeToWait, null);
+    }
+
+    public static void sleepOrCry(long timeToWait, String message) {
+        try {
+            Thread.sleep(timeToWait);
+        } catch (InterruptedException e) {
+            if (message == null)
+                throw new IllegalStateException(e);
+            throw new IllegalStateException(message, e);
+        }
+    }
+
+}
