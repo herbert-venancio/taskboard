9a0656d3ee3afc54a9e3a16bd21c7c7d50fc57e2
Herbert Weigert Venancio <herbert.venancio@objective.com.br>
2018-02-14 16:13:22 -0200

    [TOOLS-576] bugfix NPE when searching issues with subtasks

--- src/main/java/objective/taskboard/jira/client/JiraSubtaskDto.java
+++ src/main/java/objective/taskboard/jira/client/JiraSubtaskDto.java
@@ -1,16 +1,40 @@
 package objective.taskboard.jira.client;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class JiraSubtaskDto {
 
-    private JiraIssueTypeDto issuetype;
+    @JsonProperty
     private String key;
+    @JsonProperty
+    private JiraSubtaskDtoFields fields;
 
     public JiraIssueTypeDto getIssueType() {
-        return issuetype;
+        return fields.issuetype;
     }
 
     public String getIssueKey() {
         return key;
     }
 
+    private static class JiraSubtaskDtoFields {
+        public JiraIssueTypeDto issuetype;
+
+        private Map<String, JiraIssueDto.JSONObjectAdapter> other = new HashMap<>();
+
+        @JsonAnyGetter
+        public Map<String, JiraIssueDto.JSONObjectAdapter> other() {
+            return other;
+        }
+
+        @JsonAnySetter
+        public void set(String name, JiraIssueDto.JSONObjectAdapter value) {
+            other.put(name, value);
+        }
+    }
 }
