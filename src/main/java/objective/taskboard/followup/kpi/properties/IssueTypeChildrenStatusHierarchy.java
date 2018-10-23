60bcacc767dd8345888e268097728bf17cef45e1
heronoyama <heroyama2@gmail.com>
2018-10-23 13:20:07 -0300

    [TOOLS-1525] New Configurations + Cleaning

---
+++ src/main/java/objective/taskboard/followup/kpi/properties/IssueTypeChildrenStatusHierarchy.java
@@ -0,0 +1,59 @@
+package objective.taskboard.followup.kpi.properties;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class IssueTypeChildrenStatusHierarchy {
+
+    @NotEmpty
+    @Valid
+    private List<Hierarchy> hierarchies = new LinkedList<>();
+    
+    
+    public List<Hierarchy> getHierarchies() {
+        return hierarchies;
+    }
+
+    public void setHierarchies(List<Hierarchy> hierarchies) {
+        this.hierarchies = hierarchies;
+    }
+
+    public static class Hierarchy {
+
+        @NotEmpty
+        private String fatherStatus;
+
+        private List<Long> childrenTypeId = new LinkedList<>();
+        
+        private List<String> childrenStatus = new LinkedList<>();
+        
+        public String getFatherStatus() {
+            return fatherStatus;
+        }
+
+        public void setFatherStatus(String featureStatus) {
+            this.fatherStatus = featureStatus;
+        }
+        
+        public List<Long> getChildrenTypeId() {
+            return childrenTypeId;
+        }
+        
+        public void setChldrenTypeId(List<Long> childrenTypeId) {
+            this.childrenTypeId = childrenTypeId;
+        }
+
+        public List<String> getChildrenStatus() {
+            return childrenStatus;
+        }
+
+        public void setChildrenStatus(List<String> childrenStatus) {
+            this.childrenStatus = childrenStatus;
+        }
+
+    }
+}
