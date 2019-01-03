55a2ad275d9fc83f23391e7151a3bd8ee093103f
Lucas Fantacuci <lucasfantacuci@gmail.com>
2019-01-03 15:49:43 -0200

    [TOOLS-1751] Planned x Ballpark

--- kpis/src/main/java/objective/taskboard/followup/PlannedVsBallparkChartData.java
+++ kpis/src/main/java/objective/taskboard/followup/PlannedVsBallparkChartData.java
@@ -1,11 +1,17 @@
 package objective.taskboard.followup;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class PlannedVsBallparkChartData {
+
+    @JsonProperty("name")
     public String type;
+    @JsonProperty("y")
     public double totalEffort;
     
     public PlannedVsBallparkChartData(String type, double totalEffort) {
         this.type = type;
-        this.totalEffort = totalEffort;                
+        this.totalEffort = totalEffort;
     }
 }
