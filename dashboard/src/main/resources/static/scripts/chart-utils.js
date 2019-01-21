3522f83ce9d754ad951e56e460810b3c14684a07
Junior Miqueletti <juniormiqueletti@gmail.com>
2019-01-21 17:58:10 -0200

    [TOOLS-1749] Changing Chart ScopeProgress to HighCharts

--- dashboard/src/main/resources/static/scripts/chart-utils.js
+++ dashboard/src/main/resources/static/scripts/chart-utils.js
@@ -84,7 +84,7 @@ class ChartUtils {
     static resizeAllHighchartsCharts () {
         /*
          * This function performs a resize on all Highchart charts.
-         * 
+         *
          * It is necessary because when the chart is first loaded, the container
          * (div) has no size set, then the chart's size falls back to the lib's
          * default size which gives an erroneous size.
@@ -257,7 +257,7 @@ class ChartBuilderBase {
         });
         return this;
     }
-    
+
     withTooltipHeaderSubTitle (subTitle) {
         Highcharts.merge(true, this._tooltipHeader, {
             subTitle: {
@@ -266,7 +266,7 @@ class ChartBuilderBase {
         });
         return this;
     }
-    
+
     withTooltipHeaderSubTitleBold () {
         Highcharts.merge(true, this._tooltipHeader, {
             subTitle: {
@@ -307,7 +307,7 @@ class ChartBuilderBase {
         Highcharts.merge(true, this.options, {series: seriesData});
         return this;
     }
-    
+
     withPlotLineAt (date) {
         this.plotLineDate = date;
         this.hasPlotLine = true;
@@ -366,9 +366,9 @@ class PieChartBuilder extends ChartBuilderBase {
         this.options.chart.plotBorderWidth = null;
         this.options.chart.plotShadow = false;
         this.options.tooltip.reversed = false;
-        this.options.plotOptions.pie = { 
-            allowPointSelect: true, 
-            cursor: 'pointer', 
+        this.options.plotOptions.pie = {
+            allowPointSelect: true,
+            cursor: 'pointer',
             showInLegend: true,
             dataLabels: {
                 enabled: true,
@@ -404,7 +404,7 @@ class WeeklyChartBuilder extends ChartBuilderBase {
 class ProgressChartBuilder extends ChartBuilderBase {
     constructor(divID, startDate, endDate){
 
-        super(divID);		
+        super(divID);
         this.options.yAxis.max = 100;
         this.options.yAxis.labels.format = '{value}%';
         this.options.yAxis.title.text = 'Progress %';
@@ -683,5 +683,23 @@ class CycleTimeChartBuilder extends ChartBuilderBase {
                 xDateFormat: '%b %e, %Y'
             }
         });
+    }
+}
+
+class ScopeProgressChartBuilder extends ChartBuilderBase {
+    constructor(divID, startDate, xAxisPlotLines){
+
+        super(divID);
+        this.options.yAxis.title.text = 'Effort (hours)';
+        this.options.yAxis.labels.format = '{value}';
+        this.options.xAxis.labels.format = '{value:%e %b %y}';
+        this.options.xAxis.type = 'datetime';
+        this.options.tooltip.headerFormat = '';
+        this.options.tooltip.footerFormat = '';
+        this.options.tooltip.pointFormat = '<b>{series.name}: {point.y:.2f} h ({point.x:%m/%d/%Y})</b><br>';
+        this.options.tooltip.shared = false;
+        this.options.xAxis.plotLines = xAxisPlotLines;
+        this.options.plotOptions.series.pointStart = startDate;
+        this.options.plotOptions.series.pointInterval = 24 * 3600 * 1000;
     }
 }
