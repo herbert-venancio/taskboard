4291ef2932acc3ddba42f02e5cba1b3c5031e471
Jean Yuji Takano <jean.takano@objective.com.br>
2019-02-27 09:44:13 -0300

    [TOOLS-2395] Toolbar buttons with intermittent order

--- core/src/main/java/objective/taskboard/extension/ApplicationToolbarService.java
+++ core/src/main/java/objective/taskboard/extension/ApplicationToolbarService.java
@@ -1,24 +1,29 @@
 package objective.taskboard.extension;
 
-import java.util.Collections;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.unmodifiableList;
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApplicationToolbarService {
-    
+
     private final List<ApplicationToolbarItem> items;
-    
+
     @Autowired
     public ApplicationToolbarService(Optional<List<ApplicationToolbarItem>> items) {
-        this.items = Collections.unmodifiableList(items.orElse(Collections.emptyList()));
+        this.items = unmodifiableList(items.orElse(emptyList()));
     }
-    
+
     public List<ApplicationToolbarItem> getItems() {
-        return items.stream().filter(e -> e.isVisible()).collect(Collectors.toList());
+        return items.stream()
+                .filter(e -> e.isVisible())
+                .sorted(comparing(ApplicationToolbarItem::getButtonId))
+                .collect(toList());
     }
 }
