842c1da109cef66f55e12b68713d85542ecf48ec
Marco Braga <marcokuchla@gmail.com>
2019-03-08 14:46:46 -0300

    [TOOLS-2422] Fix Worklog Distribution
    Fix the distribution when a configured hierarchy by status appears before
    a hiearchy by type. Now the order in configuration does not interfere
    with the distribution results.

--- kpis/src/main/java/objective/taskboard/followup/kpi/ChildrenWorklogDistributor.java
+++ kpis/src/main/java/objective/taskboard/followup/kpi/ChildrenWorklogDistributor.java
@@ -1,15 +1,18 @@
 package objective.taskboard.followup.kpi;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import objective.taskboard.followup.kpi.properties.IssueTypeChildrenStatusHierarchy;
-import objective.taskboard.followup.kpi.properties.IssueTypeChildrenStatusHierarchy.Hierarchy;
 
 public class ChildrenWorklogDistributor {
 
-    private IssueTypeChildrenStatusHierarchy hierarchies;
+    private Map<String, TypeDistributor> types = new HashMap<>();
+    private Map<String, StatusDistributor> statuses = new HashMap<>();
     private IssueKpi issueKpi;
     private Set<ZonedWorklog> worklogsAlreadyDistributed = new HashSet<>();
 
@@ -18,39 +21,65 @@ public class ChildrenWorklogDistributor {
     }
 
     private ChildrenWorklogDistributor(IssueTypeChildrenStatusHierarchy hierarchies, IssueKpi issueKpi) {
-        this.hierarchies = hierarchies;
+        initializeDistributorsUsingIssueStatuses(issueKpi);
+        configureDistributorsUsingHiearchies(hierarchies);
         this.issueKpi = issueKpi;
     }
 
-    private void distributeWorklogs() {
-        for (Hierarchy hierarchy : hierarchies.getHierarchies()) {
-            String fatherStatus = hierarchy.getFatherStatus();
-            List<Long> childrenTypeIds = hierarchy.getChildrenTypeIds();
-            List<String> childrenStatuses = hierarchy.getChildrenStatuses();
-            issueKpi.findStatus(fatherStatus)
-                    .ifPresent(statusTransition -> {
-                        distributeWorklogsFromChildrenTypeIds(statusTransition, childrenTypeIds);
-                        distributeWorklogsFromChildrenStatus(statusTransition, childrenStatuses);
-                    });
-        }
+    private void initializeDistributorsUsingIssueStatuses(IssueKpi issueKpi) {
+        issueKpi.getStatusChain().getStatusesAsList().forEach(s -> {
+            types.put(s.getStatusName(), new TypeDistributor(s));
+            statuses.put(s.getStatusName(), new StatusDistributor(s));
+        });
     }
 
-    private void distributeWorklogsFromChildrenTypeIds(StatusTransition statusTransition, List<Long> childrenTypeIds) {
-        for (long childTypeId : childrenTypeIds) {
-            List<ZonedWorklog> worklogs = issueKpi.getWorklogFromChildrenTypeId(childTypeId);
-            for (ZonedWorklog worklog : worklogs) {
-                statusTransition.putWorklog(worklog);
-                worklogsAlreadyDistributed.add(worklog);
+    private void configureDistributorsUsingHiearchies(IssueTypeChildrenStatusHierarchy hierarchies) {
+        hierarchies.getHierarchies().stream().forEach(h -> {
+           types.get(h.getFatherStatus()).addAll(h.getChildrenTypeIds());
+           statuses.get(h.getFatherStatus()).addAll(h.getChildrenStatuses());
+        });
+    }
+
+    private void distributeWorklogs() {
+        types.values().stream().forEach(TypeDistributor::distribute);
+        statuses.values().stream().forEach(StatusDistributor::distribute);
+    }
+
+    private class TypeDistributor {
+        private StatusTransition statusTransition;
+        private List<Long> childrenTypeIds = new LinkedList<>();
+        private TypeDistributor(StatusTransition statusTransition) {
+            this.statusTransition = statusTransition;
+        }
+        private void addAll(List<Long> childrenTypeIds) {
+            this.childrenTypeIds.addAll(childrenTypeIds);
+        }
+        private void distribute() {
+            for (long childTypeId : childrenTypeIds) {
+                List<ZonedWorklog> worklogs = issueKpi.getWorklogFromChildrenTypeId(childTypeId);
+                for (ZonedWorklog worklog : worklogs) {
+                    statusTransition.putWorklog(worklog);
+                    worklogsAlreadyDistributed.add(worklog);
+                }
             }
         }
     }
-
-    private void distributeWorklogsFromChildrenStatus(StatusTransition statusTransition, List<String> childrenStatuses) {
-        for (String childStatus : childrenStatuses) {
-            List<ZonedWorklog> worklogs = issueKpi.getWorklogFromChildrenStatus(childStatus);
-            for (ZonedWorklog worklog : worklogs) {
-                if (!worklogsAlreadyDistributed.contains(worklog)) {
-                    statusTransition.putWorklog(worklog);
+   private class StatusDistributor {
+        private StatusTransition statusTransition;
+        private List<String> childrenStatuses = new LinkedList<>();
+        private StatusDistributor(StatusTransition statusTransition) {
+            this.statusTransition = statusTransition;
+        }
+        private void addAll(List<String> childrenStatuses) {
+            this.childrenStatuses.addAll(childrenStatuses);
+        }
+        private void distribute() {
+            for (String childStatus : childrenStatuses) {
+                List<ZonedWorklog> worklogs = issueKpi.getWorklogFromChildrenStatus(childStatus);
+                for (ZonedWorklog worklog : worklogs) {
+                    if (!worklogsAlreadyDistributed.contains(worklog)) {
+                        statusTransition.putWorklog(worklog);
+                    }
                 }
             }
         }
