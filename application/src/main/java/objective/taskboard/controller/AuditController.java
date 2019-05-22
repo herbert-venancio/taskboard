7dc2c202a6a2f56942c717b904260e061d48bfbc
Herbert Weigert Venancio <herbert.venancio@objective.com.br>
2019-05-22 17:20:31 -0300

    [TOOLS-2577] created AuditService: queries and restore snapshots
    - refactored AuditController

--- application/src/main/java/objective/taskboard/controller/AuditController.java
+++ application/src/main/java/objective/taskboard/controller/AuditController.java
@@ -1,109 +1,84 @@
 package objective.taskboard.controller;
 
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Optional;
 
+import static java.util.stream.Collectors.toMap;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+import org.apache.commons.lang3.StringUtils;
 import org.javers.core.Javers;
-import org.javers.core.diff.Change;
-import org.javers.core.metamodel.object.CdoSnapshot;
-import org.javers.repository.jql.QueryBuilder;
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
+import org.javers.core.json.JsonConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import objective.taskboard.data.TaskboardIssue;
-import objective.taskboard.domain.ProjectFilterConfiguration;
-import objective.taskboard.followup.cluster.SizingCluster;
-import objective.taskboard.project.config.changeRequest.ChangeRequest;
-import objective.taskboard.repository.ProjectFilterConfigurationRepository;
-import objective.taskboard.repository.TaskboardIssueRepository;
+import objective.taskboard.database.AuditService;
 
 @RestController
 @RequestMapping(value = "/audit")
 public class AuditController {
 
-    private final Javers javers;
-    private TaskboardIssueRepository issueRepository;
-    private ProjectFilterConfigurationRepository projectRepository;
+    private final AuditService auditService;
+    private final JsonConverter jsonConverter;
+    private final Map<String, Class<?>> typeNameMap;
 
     @Autowired
-    public AuditController(Javers javers, TaskboardIssueRepository issueRepository, ProjectFilterConfigurationRepository projectRepository) {
-        this.javers = javers;
-        this.issueRepository = issueRepository;
-        this.projectRepository = projectRepository;
+    public AuditController(AuditService auditService, Javers javers) {
+        this.auditService = auditService;
+        this.jsonConverter = javers.getJsonConverter();
+        typeNameMap = mapTypes(auditService.getAuditedTypes());
     }
 
-    @RequestMapping("/change-request/changes")
-    public String getChangeRequestChanges() {
-        return getChanges(ChangeRequest.class);
+    private static Map<String, Class<?>> mapTypes(Collection<Class<?>> types) {
+        return types.stream()
+                .collect(toMap(type ->
+                                StringUtils.join(
+                                        StringUtils.splitByCharacterTypeCamelCase(type.getSimpleName()),
+                                        "-")
+                                        .toLowerCase(Locale.ENGLISH)
+                        , Function.identity()));
     }
 
-    @RequestMapping("/project-filter-configuration/changes")
-    public String getProjectChanges() {
-        return getChanges(ProjectFilterConfiguration.class);
+    @GetMapping("/{type}/changes")
+    public ResponseEntity getEntityChanges(@PathVariable("type") String typeName) {
+        return Optional.ofNullable(typeNameMap.get(typeName))
+                .map(type -> ResponseEntity.ok(getChanges(type)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @RequestMapping("/sizing-cluster/changes")
-    public String getSizingChanges() {
-        return getChanges(SizingCluster.class);
+    @GetMapping("/{type}/snapshots")
+    public ResponseEntity getEntitySnapshots(@PathVariable("type") String typeName) {
+        return Optional.ofNullable(typeNameMap.get(typeName))
+                .map(type -> ResponseEntity.ok(getSnapshots(type)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @RequestMapping("/taskboard-issue/changes")
-    public String getTaskboardIssueChnages() {
-        return getChanges(TaskboardIssue.class);
+    @PostMapping("{type}/restore")
+    public ResponseEntity restoreEntity(@PathVariable("type") String typeName, @RequestBody RestoreRequest request) {
+        return Optional.ofNullable(typeNameMap.get(typeName))
+                .map(type -> restore(request, type))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @RequestMapping("/change-request/snapshots")
-    public String getChangeRequestSnapshots() {
-        return getSnapshots(ChangeRequest.class);
-    }
+    public static class RestoreRequest {
 
-    @RequestMapping("/project-filter-configuration/snapshots")
-    public String getProjectSnapshots() {
-        return getSnapshots(ProjectFilterConfiguration.class);
-    }
-
-    @RequestMapping("/sizing-cluster/snapshots")
-    public String getSizingSnapshots() {
-        return getSnapshots(SizingCluster.class);
-    }
-
-    @RequestMapping("/taskboard-issue/snapshots")
-    public String getTaskboardIssueSnapshots() {
-        return getSnapshots(TaskboardIssue.class);
-    }
-
-    @RequestMapping("/project-filter-configuration/blah")
-    public String restoreProject(@RequestBody RestoreRequest<Long> request) {
-        Optional.ofNullable(projectRepository.getOne(request.getId()))
-        .ifPresent(projectFilterConfiguration -> {
-            restore(request, projectFilterConfiguration, ProjectFilterConfiguration.class);
-        });
-        return "ok";
-    }
-
-    @RequestMapping("/taskboard-issue/blah")
-    public String restoreFromHistory(@RequestBody RestoreRequest<String> request) {
-        issueRepository.findByIssueKey(request.getId())
-        .ifPresent(taskboardIssue -> {
-            restore(request, taskboardIssue, TaskboardIssue.class);
-        });
-        return "ok";
-    }
-
-    public static class RestoreRequest<T> {
-
-        private T id;
+        private String id;
         private BigDecimal commitId;
 
-        public T getId() {
+        public String getId() {
             return id;
         }
-        public void setId(T id) {
+        public void setId(String id) {
             this.id = id;
         }
         public BigDecimal getCommitId() {
@@ -115,32 +90,19 @@ public class AuditController {
     }
 
     private String getChanges(Class<?> type) {
-        QueryBuilder jqlQuery = QueryBuilder.byClass(type);
-
-        List<Change> changes = javers.findChanges(jqlQuery.build());
-
-        return javers.getJsonConverter().toJson(changes);
+        return jsonConverter.toJson(auditService.getChanges(type));
     }
 
     private String getSnapshots(Class<?> type) {
-        QueryBuilder jqlQuery = QueryBuilder.byClass(type);
-
-        List<CdoSnapshot> changes = javers.findSnapshots(jqlQuery.build());
-
-        return javers.getJsonConverter().toJson(changes);
+        return jsonConverter.toJson(auditService.getSnapshots(type));
     }
-    
-    private <T> void restore(RestoreRequest<T> request, Object entity, Class<?> type) {
-        QueryBuilder jqlQuery = QueryBuilder.byInstanceId(request.getId(), type)
-                .withCommitId(request.getCommitId());
 
-        List<CdoSnapshot> snaps = javers.findSnapshots(jqlQuery.build());
-        if(snaps.size() != 1)
-            return;
-
-        BeanWrapper bean = new BeanWrapperImpl(entity);
-        snaps.get(0).getState().forEachProperty((property, value) -> {
-            bean.setPropertyValue(property, value);
-        });
+    private ResponseEntity restore(RestoreRequest request, Class<?> type) {
+        try {
+            auditService.restore(request.getId(), request.getCommitId(), type);
+            return ResponseEntity.ok("ok");
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
     }
 }
